20eb657125968e29a1c76b7eaad40b12c38006f2
Cat10847 <79733309+Cat10847@users.noreply.github.com>
2021-05-13 12:38:39 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,45 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Bpg9;yrnfTOA!X*8kN?6":
+            case "timerLoadingStatic":return img`
+d d d d d d 
+. d . . d . 
+. . d d . . 
+. . d d . . 
+. d . . d . 
+d d d d d d 
+`;
+            case "g-fZ`kxNnxNl2,RD_0rz":
+            case "mouseNotClicked":return img`
+. 1 1 1 1 . 
+1 1 1 1 1 1 
+1 1 1 1 1 1 
+1 1 1 1 1 1 
+1 1 1 1 1 1 
+. 1 1 1 1 . 
+`;
+            case "T41Ys30To[E-1aPAt`{3":
+            case "mouseClicked":return img`
+. . . . . . 
+. . 1 1 . . 
+. 1 1 1 1 . 
+. 1 1 1 1 . 
+. . 1 1 . . 
+. . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
